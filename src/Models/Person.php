96a9ac30969be42885e9fab51b7378e7b3b94b3e
chelsymooy <chelsymooy1108@gmail.com>
2015-05-15 16:01:02 +0700

    Update for finger sync

--- src/Models/Person.php
+++ src/Models/Person.php
@@ -116,6 +116,7 @@ class Person extends BaseModel {
 											'groupcontacts' 				=> 'GroupContacts',
 											'charttag' 						=> 'ChartTag', 
 											'branchname' 					=> 'BranchName', 
+											'displayupdatedfinger'			=> 'DisplayUpdatedFinger',
 											'requireddocuments'	 			=> 'RequiredDocuments',
 										];
 
--- src/Models/Relations/HasOne/HasFingerTrait.php
+++ src/Models/Relations/HasOne/HasFingerTrait.php
@@ -20,4 +20,15 @@ trait HasFingerTrait {
 	{
 		return $this->hasOne('ThunderID\Finger\Models\Finger');
 	}
+
+	public function scopeDisplayUpdatedFinger($query, $variable)
+	{
+		return $query->select(['contacts.value as email', 'fingers.*'])
+					->join('contacts', 'persons.id', '=', 'contacts.person_id')
+					->join('fingers', 'persons.id', '=', 'fingers.person_id')
+					->where('contacts.item', 'email')
+					->where('contacts.is_default', true)
+					->where('fingers.updated_at', '>=', $variable)
+					;
+	}
 }
